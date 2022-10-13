4e184d7cc3a0ac5052f0e3808d855a029f84ed03
marciomtk <marciomtk@gmail.com>
2022-10-12 23:09:40 -0300

    primeiro comit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const pdf = require('html-pdf');
+const ejs = require('ejs');
+const cors = require('cors');
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/gerar', (req,res) => {
+   
+   ejs.renderFile('./templates/index.ejs',{name:'MARCIO ALVES',cidade:'teixeira de freitas'},(err,html) =>{
+    if (err){
+        return res.status(500).json({message:'erro no servidor'});
+    }
+    
+    const options = {
+        format:'A4',
+        border:{
+            right:'8'
+        }
+    };
+
+    pdf.create(html,options).toFile('./uploads/report.pdf',(error,response) =>{
+        if (!error) {
+            return res.json({message: 'Sucesso'});
+        } else {
+            return res.json({message:'falha'});
+        }
+    })
+
+   });
+
+});
+
+
+app.get('/baixar',(req,res) =>{
+    res.type('pdf');
+    res.download('./uploads/report.pdf');
+});
+
+app.listen(3333);
